8bd56471b1fde19e2cd31a2bba3f711bf25d5ce1
srhm <me@srhm.ca>
2023-10-16 02:03:36 -0400

    Try to deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	}
 };
+
+config.paths = { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
+
+export default config;
