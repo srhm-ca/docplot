3bf5bb9ebb743fb0e66a855cd07e9f52a11e495c
srhm <me@srhm.ca>
2023-10-16 13:50:14 -0400

    Delete sveltekit folder

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.a2f8eb1c.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.6f232df0.js","_app/immutable/chunks/singletons.1aaa299a.js","_app/immutable/chunks/index.0378bb41.js"];
-export const stylesheets = [];
-export const fonts = [];
